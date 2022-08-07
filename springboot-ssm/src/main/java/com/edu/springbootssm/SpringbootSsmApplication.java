10387f1b96890f834b7f9007befadc086585424e
“wangzk” <zhikuan0724@163.com>
2022-08-07 19:10:35 +0800

    测试0807

--- springboot-ssm/src/main/java/com/edu/springbootssm/SpringbootSsmApplication.java
+++ springboot-ssm/src/main/java/com/edu/springbootssm/SpringbootSsmApplication.java
@@ -1,5 +1,6 @@
 package com.edu.springbootssm;
 
+import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,11 @@ public class SpringbootSsmApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(SpringbootSsmApplication.class, args);
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("aaaa");
     }
 
 }
